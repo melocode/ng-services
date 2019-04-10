909036e8f0efa2907efafe12c2821b33659f6c5f
Lemuel <lemuel@lemuel>
2019-04-10 14:02:05 -0700

    move both services to the highest level in the Hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
+import {AppComponent} from './app.component';
+import {AccountComponent} from './account/account.component';
+import {NewAccountComponent} from './new-account/new-account.component';
+import {AccountsService} from "./accounts.service";
+import {LoggingService} from "./logging.service";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { NewAccountComponent } from './new-account/new-account.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
