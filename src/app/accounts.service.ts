0e96df5e899af5cf657a311cff740de835665881
Lemuel <lemuel@lemuel>
2019-04-10 13:55:04 -0700

    add accounts service (to share common accounts array)

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  constructor() { }
+
+  addAccount(newAccount: {name: string, status: string}): void {
+    this.accounts.push(newAccount);
+  }
+
+  updateAccount(updateInfo: {id: number, newStatus: string}): void {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
