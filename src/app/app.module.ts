3b3f0985077cb61e378b7944364dd80b5189d70b
Lemuel <lemuel@lemuel>
2019-04-10 16:09:14 -0700

    use Injectable providedIn: root instead of adding to providers in app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {AccountComponent} from './account/account.component';
 import {NewAccountComponent} from './new-account/new-account.component';
-import {AccountsService} from "./services/accounts.service";
-import {LoggingService} from "./services/logging.service";
 import {AssignmentComponent} from './assignment/assignment.component';
 import {ActiveUsersComponent} from './assignment/active-users/active-users.component';
 import {InactiveUsersComponent} from './assignment/inactive-users/inactive-users.component';
@@ -26,7 +24,7 @@ import {InactiveUsersComponent} from './assignment/inactive-users/inactive-users
     FormsModule,
     HttpModule
   ],
-  providers: [AccountsService, LoggingService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
